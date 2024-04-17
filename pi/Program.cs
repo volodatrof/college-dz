79a6da7398b92a929dc2de6efde44593fc5708b6
voloda <voloda@DESKTOP-H68HFR6>
2024-04-17 22:02:11 +0300

    Add project files.

---
+++ pi/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        //Вимір кола
+        long circumference = 245850922;
+        long diameter = 78256779;
+
+        //Обчислення 
+        string piContinuedFraction = GetPiContinuedFraction(circumference, diameter);
+
+
+        Console.WriteLine(piContinuedFraction); //результат
+    }
+
+    static string GetPiContinuedFraction(long circumference, long diameter)
+    {
+        // пошук цілої част і залишку
+        long integerPart = circumference / diameter;
+        long remainder = circumference % diameter;
+
+        string continuedFraction = $"{integerPart}\n3+-----------------------------";
+
+        while (remainder != 0)
+        {
+            // Знаходимо новий діаметр
+            long newDiameter = diameter - remainder;
+
+            //нова ціла частина і залишок
+            long newIntegerPart = newDiameter / remainder;
+            remainder = newDiameter % remainder;
+
+            // нова ціла частина
+            continuedFraction += $"\n{newIntegerPart}\n{(newIntegerPart * 2 + 1)}+-----------------------------";
+
+            //додаєм "+" для початку наст кроку
+            if (remainder != 0)
+                continuedFraction += $"\n                1";
+        }
+
+        return continuedFraction;
+    }
+}
